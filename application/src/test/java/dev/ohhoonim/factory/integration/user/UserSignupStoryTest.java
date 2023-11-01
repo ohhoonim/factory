9ed704b93bd1205697d62a245f5262badf424fa1
ohhoonim <donnj95@gmail.com>
2023-11-01 05:31:53 +0000

    story test

--- application/src/test/java/dev/ohhoonim/factory/integration/user/UserSignupStoryTest.java
+++ application/src/test/java/dev/ohhoonim/factory/integration/user/UserSignupStoryTest.java
@@ -16,30 +16,32 @@ import dev.ohhoonim.factory.api.config.common.ResponseCodeEnum;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserSignupStoryTest {
 
-    @Autowired
-    TestRestTemplate restTemplate;
+        @Autowired
+        TestRestTemplate restTemplate;
 
-    @Test
-    public void userSignupBasicStory() {
-        UserSignupRequest dto = new UserSignupRequest(
-                "matthew@ohhoonim.com",
-                "1234Qwert",
-                "1234Qwert",
-                "matthew");
-        // 입력 필수값 체크
-        ResponseEntity<DefaultResponseBody> commandRequiredInputResponse = restTemplate.postForEntity(
-                "/auth/checkRequiredItem", dto, DefaultResponseBody.class);
-        assertEquals(HttpStatus.OK, commandRequiredInputResponse.getStatusCode());
+        @Test
+        public void userSignupBasicStory() {
+                UserSignupRequest dto = new UserSignupRequest(
+                                "matthew@ohhoonim.com",
+                                "1234Qwert",
+                                "1234Qwert",
+                                "matthew");
+                // 입력 필수값 체크
+                ResponseEntity<DefaultResponseBody> commandRequiredInputResponse = restTemplate.postForEntity(
+                                "/auth/checkRequiredItem", dto, DefaultResponseBody.class);
+                assertEquals(HttpStatus.OK, commandRequiredInputResponse.getStatusCode());
 
-        DefaultResponseBody body = commandRequiredInputResponse.getBody();
-        assertEquals(ResponseCodeEnum.SUCCESS, body.getCode());
+                DefaultResponseBody requiredInputResponseBody = commandRequiredInputResponse.getBody();
+                assertEquals(ResponseCodeEnum.SUCCESS, requiredInputResponseBody.getCode());
 
-        // 회원가입 요청
-        ResponseEntity<DefaultResponseBody> commandRequestSignupReponse = restTemplate.postForEntity(
-                "/auth/requestSignup", dto, DefaultResponseBody.class);
-        assertEquals(HttpStatus.OK, commandRequestSignupReponse.getStatusCode());
-
-        // todo 회원 삭제 또는 비활성 기능 구현 필요
-    }
+                // 회원가입 요청
+                ResponseEntity<DefaultResponseBody> commandRequestSignupReponse = restTemplate.postForEntity(
+                                "/auth/requestSignup", dto, DefaultResponseBody.class);
+                assertEquals(HttpStatus.OK, commandRequestSignupReponse.getStatusCode());
+                DefaultResponseBody requestSignupReponseBody = commandRequestSignupReponse.getBody();
+                assertEquals(ResponseCodeEnum.SUCCESS, requestSignupReponseBody.getCode());
+                
+                // todo 회원 삭제 또는 비활성 기능 구현 필요
+        }
 
 }
